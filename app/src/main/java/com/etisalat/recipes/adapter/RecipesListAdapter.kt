ba3ac2a8f60c1998331231325eb0de533164319f
Abdelaziz Daoud <abdelaz9z@icloud.com>
2023-10-06 17:34:45 +0300

    Use recipes.json to build a single-page application that shows a list of recipes via HTTP request

--- app/src/main/java/com/etisalat/recipes/adapter/RecipesListAdapter.kt
+++ app/src/main/java/com/etisalat/recipes/adapter/RecipesListAdapter.kt
@@ -3,6 +3,7 @@ package com.etisalat.recipes.adapter
 import android.content.Context
 import android.os.Build
 import android.view.LayoutInflater
+import android.view.View
 import android.view.ViewGroup
 import androidx.annotation.RequiresApi
 import androidx.core.content.ContextCompat
@@ -52,25 +53,26 @@ class RecipesListAdapter(
             itemView.setOnClickListener { itemClick(recipesResponseItem) }
 
             // Set the data to the views
-            binding.nameTextView.text = recipesResponseItem.name
-            binding.headlineTextView.text = recipesResponseItem.headline
-            checkDifficulty(recipesResponseItem.difficulty)
-            binding.timeTextView.text = AndroidUtils().convertDuration(recipesResponseItem.time)
-
-            // Load the image using Picasso or any other image loading library
-            Picasso.get().load(recipesResponseItem.thumb).into(binding.imageView)
+            handlingData(recipesResponseItem)
         }
 
         /**
-         * Check the difficulty level and change the background tint list and text
+         * Manipulate the data before setting the data in the existing adapter
          *
-         * 0: Very easy - color (lime green)
-         * 1: Easy - color (yellow)
-         * 2: Moderate - color (orange)
-         * 3: Difficult - color (red)
+         * @param [recipesResponseItem] recipes response item
          */
-        private fun checkDifficulty(difficultyLevel: Int) {
-            when (difficultyLevel) {
+        @RequiresApi(Build.VERSION_CODES.O)
+        private fun handlingData(recipesResponseItem: RecipesResponseItem) {
+
+            /*
+            * Check the difficulty level and change the background tint list and text
+            * 0: Very easy - color (lime green)
+            * 1: Easy - color (yellow)
+            * 2: Moderate - color (orange)
+            * 3: Difficult - color (red)
+            * */
+
+            when (recipesResponseItem.difficulty) {
                 0 -> {
                     // Set background tint color for very easy (lime green)
                     binding.difficultyTextView.backgroundTintList =
@@ -107,7 +109,44 @@ class RecipesListAdapter(
                     binding.difficultyTextView.text = context.getString(R.string.difficult)
                 }
             }
+
+            if (recipesResponseItem.calories.isEmpty()) {
+                binding.caloriesTextView.visibility = View.GONE
+            } else {
+                binding.caloriesTextView.text =
+                    context.getString(R.string.calories, recipesResponseItem.calories)
+            }
+
+            if (recipesResponseItem.carbos.isEmpty()) {
+                binding.carbosTextView.visibility = View.GONE
+            } else {
+                binding.carbosTextView.text =
+                    context.getString(R.string.carbos, recipesResponseItem.carbos)
+            }
+
+            if (recipesResponseItem.fats.isEmpty()) {
+                binding.fatsTextView.visibility = View.GONE
+            } else {
+                binding.fatsTextView.text =
+                    context.getString(R.string.fats, recipesResponseItem.fats)
+            }
+
+            if (recipesResponseItem.proteins.isEmpty()) {
+                binding.proteinsTextView.visibility = View.GONE
+            } else {
+                binding.proteinsTextView.text =
+                    context.getString(R.string.proteins, recipesResponseItem.proteins)
+            }
+
+            // Set the data to the views
+            // Load the image using Picasso or any other image loading library
+            Picasso.get().load(recipesResponseItem.thumb).into(binding.imageView)
+            binding.nameTextView.text = recipesResponseItem.name
+            binding.headlineTextView.text = recipesResponseItem.headline
+            binding.descriptionTextView.text = recipesResponseItem.description
+            binding.timeTextView.text = AndroidUtils().convertDuration(recipesResponseItem.time)
         }
+
     }
 
 }
